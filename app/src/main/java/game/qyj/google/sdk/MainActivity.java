c03f549c4fe94979be725f7f0883649f0ab89cd5
陈东奇 <chendongqi@9377.com>
2019-05-07 19:26:07 +0800

    Initial commit

---
+++ app/src/main/java/game/qyj/google/sdk/MainActivity.java
@@ -0,0 +1,287 @@
+package game.qyj.google.sdk;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.child.sdk.BaseChildApi;
+import com.common.lib.entity.CosumerData;
+import com.common.lib.entity.UserInfo;
+import com.common.lib.listener.AppLoginListener;
+import com.common.lib.listener.FloatAccountChangeListner;
+import com.common.lib.listener.LogoutListener;
+import com.common.lib.sdk.GameAction;
+import com.common.lib.utils.FastJson;
+import com.common.lib.utils.L;
+
+import static com.common.lib.sdk.Constant.ERROR_MESSAGE_GOOGLE_PAY;
+import static com.common.lib.sdk.Constant.REQUEST_CODE_LOGIN;
+import static com.common.lib.sdk.Constant.REQUEST_CODE_PAY;
+import static com.common.lib.sdk.Constant.RESULT_CODE_FACEBOOK_LOGIN_SUCCESS;
+import static com.common.lib.sdk.Constant.RESULT_CODE_GOOGLE_LOGIN_SUCCESS;
+import static com.common.lib.sdk.Constant.RESULT_CODE_GOOGLE_PAY_FAILED;
+import static com.common.lib.sdk.Constant.RESULT_CODE_GOOGLE_PAY_SUCCESS;
+import static com.common.lib.sdk.Constant.RESULT_CODE_LOGIN_SUCCESS;
+import static com.common.lib.sdk.Constant.SUCCESS_MESSAGE_LOGIN;
+
+public class MainActivity extends AppCompatActivity {
+    private BaseChildApi baseChildApi;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        baseChildApi = new BaseChildApi(this);
+        baseChildApi.active();
+
+        //注册悬浮窗切换账号监听
+        baseChildApi.setOnFloatAccountChangeListener(new FloatAccountChangeListner() {
+            @Override
+            public void onsuccess() {
+                baseChildApi.logout(new LogoutListener() {
+                    @Override
+                    public void onLogoutSuccess() {
+//                        Toast.makeText(MainActivity.this, "注销成功", Toast.LENGTH_SHORT).show();
+                        baseChildApi.checkIsLogin(MainActivity.this, new AppLoginListener() {
+                            @Override
+                            public void onLoginSuccess(String s) {
+                                baseChildApi.showFloatView(MainActivity.this);
+                            }
+
+                            @Override
+                            public void onLoginFailed(int i, String s) {
+
+                            }
+                        });
+                    }
+
+                    @Override
+                    public void onLogoutFailed(String errorMsg) {
+                        Toast.makeText(MainActivity.this, errorMsg, Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+
+            @Override
+            public void onfailed() {
+
+            }
+        });
+
+        findViewById(R.id.logout).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                baseChildApi.logout(new LogoutListener() {
+                    @Override
+                    public void onLogoutSuccess() {
+                        Toast.makeText(MainActivity.this, "注销成功", Toast.LENGTH_SHORT).show();
+                        baseChildApi.checkIsLogin(MainActivity.this, new AppLoginListener() {
+                            @Override
+                            public void onLoginSuccess(String s) {
+                                baseChildApi.showFloatView(MainActivity.this);
+                            }
+
+                            @Override
+                            public void onLoginFailed(int i, String s) {
+                                Toast.makeText(MainActivity.this, s, Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
+
+                    @Override
+                    public void onLogoutFailed(String errorMsg) {
+                        Toast.makeText(MainActivity.this, errorMsg, Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+
+        findViewById(R.id.google_pay).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String productId = "sg_1";
+                String extension = "透传参数111";//如果cp没有透传参数,请传空字符串
+
+                CosumerData cosumerData = new CosumerData();
+                cosumerData.setRole_id("3908415944392762");//角色id
+                cosumerData.setRole_name("嘎董");//角色姓名
+                cosumerData.setServer_id("63");//游戏服务器id
+
+                baseChildApi.sdkPay(MainActivity.this,productId,cosumerData,extension);
+
+            }
+        });
+
+        findViewById(R.id.wap_pay).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CosumerData cosumerData = new CosumerData();
+                cosumerData.setRole_id("3908415944392738");//角色id
+                cosumerData.setRole_name("晉↑豪");//角色姓名
+                cosumerData.setServer_id("63");//平台游戏服ID
+                cosumerData.setCpsid("909999");//cp游戏服ID
+                String extension = "透传参数";//如果cp没有透传参数,请传空字符串
+                baseChildApi.webPay(MainActivity.this,cosumerData,extension);
+            }
+        });
+
+        findViewById(R.id.sdk_register).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                baseChildApi.sdkRegister(MainActivity.this);
+            }
+        });
+
+        findViewById(R.id.sdk_login).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+//                baseChildApi.sdkLogin(MainActivity.this);
+                baseChildApi.checkIsLogin(MainActivity.this, new AppLoginListener() {
+                    @Override
+                    public void onLoginSuccess(String s) {
+                        baseChildApi.showFloatView(MainActivity.this);
+                        L.e("登陆成功--->", s);
+                        UserInfo userInfo = (UserInfo) FastJson.pareseObject(s, UserInfo.class);
+                    }
+
+                    @Override
+                    public void onLoginFailed(int i, String s) {
+                        L.e("登陆失败--->", s);
+                    }
+                });
+
+            }
+        });
+
+        findViewById(R.id.email_verify).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                baseChildApi.emailVerify(MainActivity.this);
+            }
+        });
+
+//        baseChildApi.checkIsLogin(this, new AppLoginListener() {
+//            @Override
+//            public void onLoginSuccess(String data) {
+//                baseChildApi.showFloatView(MainActivity.this);
+////                L.e("登陆成功--->", data);
+//            }
+//
+//            @Override
+//            public void onLoginFailed(int code, String msg) {
+//                Toast.makeText(MainActivity.this, "登陆失败", Toast.LENGTH_SHORT).show();
+//                L.e("登陆失败--->", msg);
+//            }
+//        });
+
+
+        submmitInfo();
+
+//        submmitPayInfo();
+
+    }
+
+//    private void submmitPayInfo() {
+//        ProductData productData = new ProductData();
+//        productData.setPrice_amount("590");//商品价格,只能填数值不要带货币单位
+//        productData.setId("hy_590");//product_id
+//        productData.setPrice_currency_code("TWD");//currency_code,货币单位
+//        baseChildApi.submitPayInfo(this,productData);
+//    }
+
+    private void submmitInfo() {
+        CosumerData cosumerData = new CosumerData();
+        cosumerData.setRole_id("3908415944392762");//角色id
+        cosumerData.setRole_name("嘎董");//角色姓名
+        cosumerData.setRole_level("20");//角色等级
+        cosumerData.setServer_id("63");//平台游戏服ID
+        cosumerData.setCpsid("909999");//cp游戏服ID
+
+//        baseChildApi.submitUserInfo(this, GameAction.ENTER_SERVER,cosumerData);
+//        baseChildApi.submitUserInfo(this, GameAction.CREATE_ROLE,cosumerData);
+//        baseChildApi.submitUserInfo(this, GameAction.LEVEL_ACHEVIED_65,cosumerData);
+//        baseChildApi.submitUserInfo(this, GameAction.LEVEL_ACHEVIED_100,cosumerData);
+//        baseChildApi.submitUserInfo(this, GameAction.LEVEL_ACHEVIED_150,cosumerData);
+        baseChildApi.submitUserInfo(this, GameAction.LEVEL_ACHEVIED,cosumerData);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case REQUEST_CODE_LOGIN: //sdk登陆註冊
+                onLoginResult(resultCode, data);
+                break;
+            case REQUEST_CODE_PAY: //sdk支付
+                onPayResult(resultCode,data);
+                break;
+            default:
+                break;
+        }
+
+    }
+
+    private void onPayResult(int resultCode,Intent data) {
+        switch (resultCode) {
+            case RESULT_CODE_GOOGLE_PAY_SUCCESS:
+                Toast.makeText(MainActivity.this,"支付校驗成功", Toast.LENGTH_SHORT).show();
+                break;
+            case RESULT_CODE_GOOGLE_PAY_FAILED:
+                String errorMsg = "支付校驗失敗";
+                if (data!=null&&data.hasExtra(ERROR_MESSAGE_GOOGLE_PAY)) {
+                    errorMsg = data.getStringExtra(ERROR_MESSAGE_GOOGLE_PAY);
+                }
+//                Toast.makeText(MainActivity.this,errorMsg, Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void onLoginResult(int resultCode, Intent data) {
+        switch (resultCode) {
+            case RESULT_CODE_LOGIN_SUCCESS://账号密码登陆成功
+
+            case RESULT_CODE_FACEBOOK_LOGIN_SUCCESS://facebook登陆成功
+
+            case RESULT_CODE_GOOGLE_LOGIN_SUCCESS://google登陆成功
+                if (data.hasExtra(SUCCESS_MESSAGE_LOGIN)) {
+                    String msg = data.getStringExtra(SUCCESS_MESSAGE_LOGIN);
+                    UserInfo userInfo = (UserInfo) FastJson.pareseObject(msg, UserInfo.class);
+                }
+                baseChildApi.showFloatView(this);
+                break;
+            default:
+                break;
+        }
+
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onDestroy() {
+
+        baseChildApi.destroySDK();
+
+        super.onDestroy();
+    }
+
+}
---
+++ app/src/main/java/game/qyj/google/sdk/SplashActivity.java
@@ -0,0 +1,22 @@
+package game.qyj.google.sdk;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+
+/**
+ * Created by Administrator on 2018/12/20.
+ */
+
+public class SplashActivity extends com.child.sdk.SplashActivity{
+    @Override
+    public void onSplashBefore(Context context, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onSplashFinish() {
+        startActivity(new Intent(SplashActivity.this,MainActivity.class));
+        finish();
+    }
+}
